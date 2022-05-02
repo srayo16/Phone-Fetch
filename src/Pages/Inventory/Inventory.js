7c0aea8ccde7255377ab6c574a8be941916cfd3a
srayo16 <srayosikder@gmail.com>
2022-05-02 10:22:30 +0600

    error handling

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -3,23 +3,29 @@ import { Link } from 'react-router-dom';
 import InventoryPro from './InventoryPro';
 import { BsArrowRight } from 'react-icons/bs';
 import UseItems from '../../Hooks/UseItems';
+import Spinners from '../Spinners';
 
 const Inventory = () => {
 
     const [items] = UseItems();
 
+    if (items.length <= 0) {
+        return <Spinners></Spinners>
+    }
+    // console.log(items);
+    
     return (
 
         <div className='container overflow-hidden' id='inventory'>
             <h1 className='text-primary text-center fw-bolder mt-5 mb-5 pt-3'>Inventory Stock Items</h1>
             <div className='row gx-5 gy-5'>
                 {
-                    items.slice(0,6).map(item => <InventoryPro key={item._id} item={item}></InventoryPro>)
+                    items.slice(0, 6).map(item => <InventoryPro key={item._id} item={item}></InventoryPro>)
                 }
             </div>
-           <div className='text-center mt-4 mb-5'>
-           <Link to='/manageinventoreis' className='text-decoration-none fw-bold fst-italic fs-5z'>Manage Inventories <BsArrowRight></BsArrowRight></Link>
-           </div>
+            <div className='text-center mt-4 mb-5'>
+                <Link to='/manageinventoreis' className='text-decoration-none fw-bold fst-italic fs-5z'>Manage Inventories <BsArrowRight></BsArrowRight></Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Firebase.init.js
+++ src/Pages/Shared/Firebase.init.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDkD_pf8s-U4b8bxAzGjWdER2i1pQ_b7OM",
-  authDomain: "phoen-tech.firebaseapp.com",
-  projectId: "phoen-tech",
-  storageBucket: "phoen-tech.appspot.com",
-  messagingSenderId: "15621281919",
-  appId: "1:15621281919:web:d3da33cc02c27b523c074a"
+  authDomain: process.env.REACT_APP_authDomain,
+  apiKey: process.env.REACT_APP_apiKey,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
